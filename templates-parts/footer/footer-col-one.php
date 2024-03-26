808a8f77c2801a48a4b2bd8e48fb13f5d1e0300d
Creastv <creastv@gail.com>
2024-03-26 16:45:25 +0100

    update

--- templates-parts/footer/footer-col-one.php
+++ templates-parts/footer/footer-col-one.php
@@ -1,28 +1,26 @@
 <div class="col">
     <?php if (is_active_sidebar('footer-1')) : ?>
-    <?php dynamic_sidebar('footer-1'); ?>
+        <?php dynamic_sidebar('footer-1'); ?>
     <?php else : ?>
-    <div class="f_company">
-        <div class="logo">
-            <svg xmlns="http://www.w3.org/2000/svg" width="102.505" height="124.648" viewBox="0 0 102.505 124.648">
-                <path id="logo"
-                    d="M191.982,4218.54v-96.2H294.488v96.2l-51.253,28.444Zm3.5-2.082,47.75,26.5,47.751-26.5v-90.6h-95.5Zm32.264,5.093v-36.187H221.4v32.66l-12.707-7.073v-70.364l6.354-6.4H234.1l6.352,6.4V4228.6ZM221.4,4172.57h6.354v-25.587H221.4Zm24.619,12.846h12.707v16.319l6.354-3.526v-19.191H252.368l-6.354-6.4v-31.984l6.354-6.4h19.061l6.354,6.4v19.191H265.075v-12.793h-6.354v19.189h12.707l6.354,6.4v38.121l-31.768,17.64Z"
-                    transform="translate(-191.982 -4122.336)" fill="#185ab1" />
-            </svg>
-        </div>
-        <div class="desc">
-            <p>Największy w historii Ministerstwa Sportu i Turystyki Program skierowany do dzieci, młodzieży oraz
-                dorosłych! </p>
-            <div class="con">
-                <div class="con__left">
-                    <a href="mailto:kontact@aktywnaszkola.pl"><b>M:</b> kontact@aktywnaszkola.pl</a>
-                    <a href="tel:0048537255791"><b>T:</b>+48 537 255 791</a>
-                </div>
-                <div class="con__right">
-                    <a href="/index.php/kontakt/" class="btn-rev">Napisz do nas</a>
+        <div class="f_company">
+            <div class="logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="102.505" height="124.648" viewBox="0 0 102.505 124.648">
+                    <path id="logo" d="M191.982,4218.54v-96.2H294.488v96.2l-51.253,28.444Zm3.5-2.082,47.75,26.5,47.751-26.5v-90.6h-95.5Zm32.264,5.093v-36.187H221.4v32.66l-12.707-7.073v-70.364l6.354-6.4H234.1l6.352,6.4V4228.6ZM221.4,4172.57h6.354v-25.587H221.4Zm24.619,12.846h12.707v16.319l6.354-3.526v-19.191H252.368l-6.354-6.4v-31.984l6.354-6.4h19.061l6.354,6.4v19.191H265.075v-12.793h-6.354v19.189h12.707l6.354,6.4v38.121l-31.768,17.64Z" transform="translate(-191.982 -4122.336)" fill="#185ab1" />
+                </svg>
+            </div>
+            <div class="desc">
+                <p>Największy w historii Ministerstwa Sportu i Turystyki Program skierowany do dzieci, młodzieży oraz
+                    dorosłych! </p>
+                <div class="con">
+                    <div class="con__left">
+                        <a href="mailto:kontact@aktywnaszkola.pl"><b>M:</b> kontact@aktywnaszkola.pl</a>
+                        <a href="tel:0048537255791"><b>T:</b> +48 537 255 791</a>
+                    </div>
+                    <div class="con__right">
+                        <a href="/index.php/kontakt/" class="btn-rev">Napisz do nas</a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     <?php endif; ?>
 </div>
