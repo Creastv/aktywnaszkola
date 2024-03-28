1eb5adf25913a3d45b23c5e582b6262033d37c30
Creastv <creastv@gail.com>
2024-03-28 10:39:44 +0100

    update

--- footer.php
+++ footer.php
@@ -2,8 +2,7 @@
 </div>
 </main>
 <svg class="f-ornament" xmlns="http://www.w3.org/2000/svg" width="1922" height="119" viewBox="0 0 1922 119">
-    <path id="Rectángulo_11" data-name="Rectángulo 11" d="M1920,3983-2,4093v9H1920Z" transform="translate(2 -3983)"
-        fill="#dff5ff" />
+    <path id="Rectángulo_11" data-name="Rectángulo 11" d="M1920,3983-2,4093v9H1920Z" transform="translate(2 -3983)" fill="#dff5ff" />
 </svg>
 
 <footer id="footer" itemscope itemtype="http://schema.org/WPFooter">
@@ -19,10 +18,10 @@
 
 </footer>
 <?php get_template_part('templates-parts/parts/sticky_social_media'); ?>
+<?php get_template_part('templates-parts/parts/modal-contact'); ?>
 <span id="go-to-top">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <path id="Icon_material-arrow-upward" data-name="Icon material-arrow-upward"
-            d="M6,18l2.115,2.115,8.385-8.37V30h3V11.745l8.37,8.385L30,18,18,6Z" transform="translate(-6 -6)" />
+        <path id="Icon_material-arrow-upward" data-name="Icon material-arrow-upward" d="M6,18l2.115,2.115,8.385-8.37V30h3V11.745l8.37,8.385L30,18,18,6Z" transform="translate(-6 -6)" />
     </svg>
 </span>
 <?php wp_footer(); ?>
