73310928d105d783cd6c01f1d36777e6381696e6
Creastv <creastv@gail.com>
2024-04-02 14:54:01 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php get_header(); ?>
-
-<?php if (have_posts()) : ?>
 <div class="cat-nav">
     <?php
     $temp_menu = wp_nav_menu(
@@ -10,21 +8,23 @@
         )
     ); ?>
 </div>
-<div class="posts-wraper">
-    <?php while (have_posts()) : the_post();
+<?php if (have_posts()) : ?>
+
+    <div class="posts-wraper">
+        <?php while (have_posts()) : the_post();
             get_template_part('templates-parts/content/content-cart-post');
         endwhile; ?>
-</div>
-<?php if (paginate_links()) { ?>
-<div class="go-pagination">
-    <?php pagination_bars(); ?>
-</div>
-<?php } ?>
+    </div>
+    <?php if (paginate_links()) { ?>
+        <div class="go-pagination">
+            <?php pagination_bars(); ?>
+        </div>
+    <?php } ?>
 
 <?php else : ?>
-<header class="entry-header">
-    <h1 class='text-center'><?php _e('Nic nie znaleziono', 'go'); ?></h1>
-</header>
+    <header class="entry-header">
+        <h1 class='text-center'><?php _e('Nic nie znaleziono', 'go'); ?></h1>
+    </header>
 <?php endif; ?>
 
 <?php get_footer();
