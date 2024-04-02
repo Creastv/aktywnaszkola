4a0aa79a78d7b5d2e26c285179106d7398178fd9
Creastv <creastv@gail.com>
2024-04-02 14:03:35 +0200

    update

--- templates-parts/header/header-cta.php
+++ templates-parts/header/header-cta.php
@@ -1,12 +1,14 @@
 <div class="navbar__cta">
     <div class="navbar__cta__logos">
         <a href="https://orlysportu.pl/" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/src/img/orly-logo-header.png" alt="Orły Sportu - Fundacja">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/img/orly-logo-header.png"
+                alt="Orły Sportu - Fundacja">
         </a>
         <a href="https://www.gov.pl/web/sport" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/src/img/ministerstwo-sportu-turystyki-logo-header.png" alt="Ministerstwo Sportu i Turystyki">
+            <img src="<?php echo get_template_directory_uri(); ?>/src/img/ministerstwo-sportu-turystyki-logo-header.png"
+                alt="Ministerstwo Sportu i Turystyki">
         </a>
     </div>
-    <a href="https://system.aktywnaszkola.pl/login" target="_blank" class="btn-main">Rejestracja</a>
+    <a href="https://system.aktywnaszkola.pl/login" target="_blank" class="btn-main"><span>Rejestracja</span></a>
     <!-- <a href="#" class="btn-main js-opener-modal"><span>bądź na bieżąco</span></a> -->
 </div>
